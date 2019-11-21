fd38bd048acfd3763c9ef893a431b1e74a26acb9
Jasmine Breeze <jasmine.breeze@bbc.co.uk>
2019-11-21 12:58:43 +0000

    task 3,4 and 5 complete, getTotalSubjects, checkIngredients, duplicateNumbers

--- challenges/week3.js
+++ challenges/week3.js
@@ -5,45 +5,63 @@ function getSquares(nums) {
   });
   return sqNum;
 }
-  
+
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   var result = "";
-for (let i=0; i<words.length; i++){
-  var String = words[i].toLowerCase();
-  if (i !=0) {
-    String = String.substr(0,1).toUpperCase() + String.substr(1);
+  for (let i = 0; i < words.length; i++) {
+    var String = words[i].toLowerCase();
+    if (i != 0) {
+      String = String.substr(0, 1).toUpperCase() + String.substr(1);
+    }
+    result += String
   }
- result +=String }
 
- return result;
+  return result;
 }
 
-//if index is zero reurn work lower case, else upeprcase first character and then join
-  
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let sum=0;
+  for (let i= 0; i < people.length; i++){
+    sum +=people[i].subjects.length;
+  }
+  return sum
 }
 
-function checkIngredients(menu, ingredient) {
-  if (menu === undefined) throw new Error("menu is required");
-  if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
-}
 
-function duplicateNumbers(arr1, arr2) {
-  if (arr1 === undefined) throw new Error("arr1 is required");
-  if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
-}
 
-module.exports = {
-  getSquares,
-  camelCaseWords,
-  getTotalSubjects,
-  checkIngredients,
-  duplicateNumbers
-};
+  function checkIngredients(menu, ingredient) {
+    if (menu === undefined) throw new Error("menu is required");
+    if (!ingredient) throw new Error("ingredient is required");
+    for (let i = 0; i < menu.length; i++){
+      if (menu[i].ingredients.includes(ingredient)){
+        return true
+      }
+    }
+    return false
+  }
+
+
+    function duplicateNumbers(arr1, arr2) {
+      if (arr1 === undefined) throw new Error("arr1 is required");
+      if (arr2 === undefined) throw new Error("arr2 is required");
+      let matches = [];
+       for (let i =0; i < arr1.length; i++){
+         for (let j = 0; j < arr2.length; j++){
+           if (arr1[i] === arr2[j]) matches.push(arr1[i])
+         }
+       }
+       let unique = [...new Set(matches)]
+       return unique.sort()
+    }
+
+    module.exports = {
+      getSquares,
+      camelCaseWords,
+      getTotalSubjects,
+      checkIngredients,
+      duplicateNumbers
+    };
