33c7c686b1e2a256c525ba40cd6508ce53255b29
Jasmine Breeze <jasmine.breeze@bbc.co.uk>
2019-11-27 17:04:38 +0000

    solved function 1. findSmallNums, functio 4. getIntegers, function 5. getCitiesn

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,28 +1,49 @@
 function findSmallNums(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+  if (!nums) throw new Error("nums is required")
+
+const SmallNum = nums.filter(function (n) {
+  return n <1;
+});
+
+return SmallNum
 }
+
+ 
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  const NamesChar = names.filter(function (n, c) {
+    return n[0] === c;
+  });
+  return NamesChar
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  const integers = nums.filter(function (n)
+  {
+    return  n % 1 === 0;
+  });
+  return integers
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+ 
+  let finalUsers = [];
+
+  for (let i = 0; i <users.length; i++) {
+    finalUsers.push(users[i].data.city.displayName);
+  }
+  return finalUsers
 }
+
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
